ac2b3fd9d737f4905309b0989db9c3a68c30a7b5
Aleksandr Kolobkov <aleksandr.kolobkov@opensoftdev.ru>
2014-11-11 19:42:06 +0400

    Mistakes Correction

--- src/Bookshelf/Controller/UserController.php
+++ src/Bookshelf/Controller/UserController.php
@@ -1,28 +1,152 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aleksandr.kolobkov
- * Date: 20.10.2014
- * Time: 16:21
- */
+
 namespace Bookshelf\Controller;
 
+use Bookshelf\Core\Logger\Logger;
+use Bookshelf\Core\Request;
+use Bookshelf\Core\Session;
 use Bookshelf\Core\Templater;
+use Bookshelf\Model\User;
+use Bookshelf\Core\Validation\Constraint\EmailConstraint;
+use Bookshelf\Core\Validation\Constraint\AlphabeticalConstraint;
+use Bookshelf\Core\Validation\Constraint\NotBlankConstraint;
+use Bookshelf\Core\Validation\Constraint\ConfirmConstraint;
+use Bookshelf\Core\Validation\Validator;
 
+
+/**
+ * @author Aleksandr Kolobkov
+ */
 class UserController
 {
+    /**
+     * Property for templater class instance
+     *
+     * @var Templater
+     */
     private $templater;
 
+    /**
+     * Property for session class instance
+     *
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @var Request
+     */
+    private $request;
+
+    private $logger;
+
+    /**
+     * Method that create templater and session class instance then execute
+     */
     public function __construct()
     {
+        $this->logger = new Logger('../logs/');
+        $this->session = new Session();
         $this->templater = new Templater();
+        $this->request = new Request($_GET, $_POST);
     }
 
-
-    public function addAction()
+    /**
+     * Default method that show user account page
+     */
+    public function defaultAction()
     {
-        $this->templater->show('User', 'Add', null);
+        $email = $this->session->get('email');
+        if (!empty($email)) {
+            $user = User::findOneBy(['email' => $email]);
+            $user->getContacts();
+            $this->templater->show('User', 'AccountPage', $user);
+        } else {
+            header("Location: /login");
+            exit;
+        }
     }
 
+    /**
+     * Method that show page for change user data
+     */
+    public function showAction()
+    {
+        $user = User::findOneBy(['id' => $this->request->get('id')]);
+        $this->templater->show('User', 'ChangeData', ['user' => $user]);
+    }
 
+    /**
+     * Method that update user data after changing
+     */
+    public function changeUserDataAction()
+    {
+        $user = new User();
+        $user->setFirstName($this->request->get('firstname'));
+        $user->setEmail($this->request->get('email'));
+        $user->setLastName($this->request->get('lastname'));
+        $user->setPassword($this->request->get('password'));
+        $errorArray = $this->changeDataValidation($user);
+
+        $userFromDb = User::findOneBy(['email' => $this->session->get('email')]);
+        $userFromDb->getContacts();
+        $user->setPassword($this->changePassword($userFromDb));
+        $params['user'] = $user;
+        if ($errorArray) {
+            $params['user'] = $userFromDb;
+            $params['errors'] = $errorArray;
+            $this->templater->show('User', 'ChangeData', $params);
+        }
+        $this->session->set('email', $this->request->get('email'));
+        $this->session->set('firstname', $this->request->get('firstname'));
+        if ($user->save()) {
+            header("Location: /user");
+            exit;
+        } else {
+            $params['errors']['save_fail'][] = 'Произошёл сбой при попытке сменить данные пользователя. Пожалуйста повторите попытку позднее';
+            $this->logger->emergency('Cant save user in DataBase');
+            return $this->templater->show('User', 'ChangeData', $params);
+        }
+    }
+
+    /**
+     * Method that used constraints from array for data validate
+     *
+     * @param $constraints array
+     * @return array
+     */
+    private function validate($constraints)
+    {
+        $validator = new Validator();
+        foreach ($constraints as $constraint) {
+            $validator->addConstraint($constraint);
+        }
+        return $validator->validate();
+    }
+
+    private function changeDataValidation($user)
+    {
+        $constraints = [
+            'firstname' => new AlphabeticalConstraint($user, 'firstname', "Имя должно состоять только из букв"),
+            'lastname' => new AlphabeticalConstraint($user, 'lastname', "Фамилия должна состоять только из букв"),
+            'firstnameBlank' => new NotBlankConstraint($user, 'firstname'),
+            'lastnameBlank' => new NotBlankConstraint($user, 'lastname'),
+            'emailName' => new EmailConstraint($user, 'email'),
+            'emailBlank' => new NotBlankConstraint($user, 'email'),
+            'passwordConfirm' => new ConfirmConstraint($user, $this->request->get('confirm_password'), 'password')
+        ];
+
+        return $this->validate($constraints);
+    }
+
+    private function changePassword($userFromDb)
+    {
+        $confirmPassword = $this->request->get('confirm_password');
+        $password = $this->request->get('password');
+        if (empty($password) && empty($confirmPassword)) {
+            return $userFromDb->getPassword();
+        } else {
+            return $this->request->get('password');
+        }
+    }
 }
